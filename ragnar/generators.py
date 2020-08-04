d3ea255e088c52cda2110fe527c788f3e98a701f
juanmcristobal <juanmcristobal@gmail.com>
2020-08-04 21:29:36 +0200

    New generators: * strip string * read csv files

--- ragnar/generators.py
+++ ragnar/generators.py
@@ -1,5 +1,18 @@
+import csv
+
+
+def gen_strip(cad):
+    return cad.strip()
+
 
 def read_file(filename):
     with open(filename) as f:
         for line in f:
             yield line
+
+
+def read_csv(filename):
+    with open(filename, "r") as csvfile:
+        datareader = csv.reader(csvfile)
+        for row in datareader:
+            yield row
